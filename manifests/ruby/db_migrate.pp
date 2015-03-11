9377d9935074c22b262c49aa0d3d6a7fa3bcc72f
martent <marten@thavenius.se>
2015-03-11 13:27:12 +0100

    Refactoring

--- manifests/ruby/db_migrate.pp
+++ manifests/ruby/db_migrate.pp
@@ -1,15 +1,11 @@
-define mcommons::ruby::db_migrate(
-  $envs = ['development', 'test']
-) {
+define mcommons::ruby::db_migrate() {
   require ::mcommons::ruby
   require ::mcommons::mysql
 
-  $envs.each |$env| {
-    exec { "Migrate database ${env}":
-      command => "bundle exec rake db:migrate RAILS_ENV=${env}",
-      user    => $::runner_name,
-      path    => $::runner_path,
-      cwd     => $::app_home,
-    }
+  exec { "Migrate database ${name}":
+    command => "bundle exec rake db:migrate RAILS_ENV=${name}",
+    user    => $::runner_name,
+    path    => $::runner_path,
+    cwd     => $::app_home,
   }
 }

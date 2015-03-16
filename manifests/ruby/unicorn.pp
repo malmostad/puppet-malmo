5913d9cf96603c075687742a107cc35ae5448063
martent <marten@thavenius.se>
2015-03-16 22:04:13 +0100

    snake_casing init script name

--- manifests/ruby/unicorn.pp
+++ manifests/ruby/unicorn.pp
@@ -3,7 +3,7 @@ class mcommons::ruby::unicorn() {
 
   # Parse and copy init.d script
   file { "unicorn-init":
-    path    => "/etc/init.d/unicorn-${::app_name}",
+    path    => "/etc/init.d/unicorn_${::app_name}",
     owner   => 'root',
     group   => 'root',
     mode    => '0755',

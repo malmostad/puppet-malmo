1058ce453dc7f6866c6595bf466869b85c36c7c3
martent <marten@thavenius.se>
2015-03-20 11:39:17 +0100

    Added unicorn to reboot tasks

--- manifests/ruby/unicorn.pp
+++ manifests/ruby/unicorn.pp
@@ -8,5 +8,9 @@ class mcommons::ruby::unicorn() {
     group   => 'root',
     mode    => '0755',
     content => template('mcommons/unicorn.erb'),
+  } ->
+
+  exec { 'Add unicorn to reboot tasks':
+    command => "/usr/sbin/update-rc.d unicorn_${::app_name} defaults",
   }
 }

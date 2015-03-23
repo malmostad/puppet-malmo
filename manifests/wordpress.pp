08c7f6c24d31fb9486d743869fec065cd91f4a41
martent <marten@thavenius.se>
2015-03-23 15:17:29 +0100

    Wordpress configuration

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,75 +1,73 @@
 class mcommons::wordpress(
   $tar_gz_url = 'https://sv.wordpress.org/latest-sv_SE.tar.gz',
 ) {
-  # require ::mcommons::apache
+  require ::mcommons::apache
 
   $archive = 'wordpress.tar.gz'
 
   exec { "Download Wordpress from ${$tar_gz_url}":
     command => "/usr/bin/wget ${$tar_gz_url} -O ${archive}",
     cwd     => $::runner_home,
-  } ->
+  }
 
-  exec { 'Extract Wordpress':
+  -> exec { 'Extract Wordpress':
     command => "/bin/tar zxvf ${archive}",
     cwd     => $::runner_home,
-    user    => $::runner_name,
-    group   => $::runner_group,
-  } ->
+  }
 
-  exec { "Delete ${archive}":
+  -> exec { 'Cleanup installer':
     command => "/bin/rm ${archive}",
     cwd     => $::runner_home,
-  } ->
+  }
 
-  # Parse and copy init.d script
-  file { 'Add Wordpress config file':
+  # Parse and copy wp-config
+  -> file { 'Add Wordpress config file':
     path    => "${::app_home}/wp-config.php",
-    owner   => $::runner_name,
-    group   => $::runner_group,
     mode    => '0644',
     content => template('mcommons/wp-config.php.erb'),
-  } ->
+  }
 
-  # exec { 'Change ownership of wordpress uploads dir':
-  #   command => "/bin/chown -R www-data:www-data ${::app_home}/wp-content/uploads",
-  # } ->
+  -> exec { 'Set owner':
+    command => "/bin/chown -R ${::runner_name}:${::runner_group} ./wordpress",
+    cwd     => $::runner_home,
+  }
 
-  # file { "${::runner_home}/wordpress-uploads":
-  #   ensure => 'directory',
-  # } ->
+  # Create uploads dir if not there
+  -> file { "${::runner_home}/wordpress-uploads":
+    ensure => 'directory',
+    owner  => 'www-data',
+    group  => 'www-data',
+  }
 
   # Symlink uploads directory
-  file { "${::runner_home}/wordpress-uploads":
+  -> file { "${::app_home}/wp-content/uploads":
     ensure => 'link',
-    force  => true,
-    target => "${::app_home}/wp-content/uploads",
+    target => "${::runner_home}/wordpress-uploads",
     owner  => $::runner_name,
     group  => $::runner_group,
-  } ->
+  }
 
-  # file { "${::runner_home}/wordpress-themes":
-  #   ensure => 'directory',
-  # } ->
-
-  # Symlink uploads directory
-  file { "${::runner_home}/wordpress-themes":
-    ensure => 'link',
-    force  => true,
-    target => "${::app_home}/wp-content/themes",
+  # Capistrano catalog structure
+  -> file { ["${::runner_home}/wordpress-deployed", "${::runner_home}/wordpress-deployed/current"]:
+    ensure => 'directory',
     owner  => $::runner_name,
     group  => $::runner_group,
-  } ->
+  }
 
-  # file { "${::runner_home}/wordpress-plugins":
-  #   ensure => 'directory',
-  # } ->
-
-  # Symlink uploads directory
-  file { "${::runner_home}/wordpress-plugins":
+  # Symlink themes directory
+  -> file { "${::app_home}/´wp-content/themes":
     ensure => 'link',
     force  => true,
-    target => "${::app_home}/wp-content/plugins",
+    target => "${::runner_home}/wordpress-deployed/current/themes",
+    owner  => $::runner_name,
+    group  => $::runner_group,
+  }
+
+  # Symlink plugins directory
+  -> file { "${::app_home}/wp-content/plugins":
+    ensure => 'link',
+    force  => true,
+    target => "${::runner_home}/wordpress-deployed/current/plugins",
     owner  => $::runner_name,
     group  => $::runner_group,
   }
